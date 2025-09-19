665e377b040f331530a87a94928b04b79bbc2bc3
Meruyert Zhunussova <m.zhunussova@globerce.capital>
2025-09-19 18:54:00 +0500

    fix files

--- extensions/tagfilter.h
+++ extensions/tagfilter.h
@@ -1,6 +1,7 @@
 #ifndef CMARK_GFM_TAGFILTER_H
 #define CMARK_GFM_TAGFILTER_H
 
+#include <cmark-gfm.h>
 #include "cmark-gfm-core-extensions.h"
 
 cmark_syntax_extension *create_tagfilter_extension(void);
